f07092dd8b7f6080d50e0d20c3f457e79b1f70d7
Gordon Fleetwood <gfleetwood@protonmail.com>
2020-09-18 07:22:55 -0400

    Update

--- R/experimental.R
+++ R/experimental.R
@@ -6,3 +6,33 @@ check_key <- function(dict, key){
 
 }
 
+create_db_doc_template <- function(con){
+
+    tbls <- odbc::dbListTables(con)
+    # DBI::dbReadTable(con_pg, tbls[1])
+
+    docs_template <- data.frame(tables = tbls, stringsAsFactors = FALSE) %>%
+        mutate(schema = "public") %>%
+        select(schema, tables) %>%
+        purrrlyr::by_row(
+            function(x) dbListFields(con, pull(x, tables)),
+            .collate = "rows",
+            .to = "column"
+        ) %>%
+        select(-.row) %>%
+        mutate(description = "")
+
+    return(docs_template)
+
+}
+
+extract_json <- function(df){
+
+    result <- df %>%
+        mutate(
+            col_new = unlist(future_map(col, ~ check_key(.x, "")))
+        )
+
+    return(result)
+
+}
