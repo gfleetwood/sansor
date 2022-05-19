54f46ee1435e43d979d316e3c1e4bf60c76ef061
Gordon Fleetwood <gfleetwood@protonmail.com>
2022-05-18 23:05:13 -0400

    Update

--- R/read_write.R
+++ R/read_write.R
@@ -19,3 +19,33 @@ create_tbl = function(data, schema_name, tbl_name){
     return(TRUE)
 
 }
+
+#' @title Read Data Thru Tunnel
+#' @description U
+#' @param con DB connection
+#' @param schema Schema name
+#' @param tbl_path The path to the table
+#' @return The df variable without the anomalous columns
+#' @export
+
+
+read_db_data_thru_tunnel = function(qry){
+   
+   cmd = Sys.getenv("TUNNEL")
+   rp = r_bg(function(x) system(x), args = list(cmd))
+   
+   con = dbConnect(
+     odbc(), 
+     Driver = Sys.getenv("DRIVER"),
+     Database = "data_warehouse",
+     Port = "5439", 
+     Server = Sys.getenv("SERVER"), 
+     UID = Sys.getenv("UN"), 
+     PWD = Sys.getenv("PW")
+   )
+   
+   df = dbGetQuery(con, qry)
+   rp$kill()
+   df
+   
+}
